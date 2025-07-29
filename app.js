d22d5ae4af8f9d1fd460e1b16a27798f7e9c440a
Ethan Huston <71285039+EthanHuston@users.noreply.github.com>
2025-07-29 13:54:04 -0400

    Implementing UI interactions for the game
    - Added a visual UI for the game.
    - Game begins with button start and can be reset
    mid playthrough.
    - Player names can be given but are rest every new
    game or reset

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  */
 function GameBoard() {
   const grid = 3;
-  const board = [];
+  let board = [];
 
   //Fill board with initial values
   for (let i = 0; i < grid; i++) {
@@ -32,8 +32,17 @@ function GameBoard() {
     );
     console.log(boardWithCellValues);
   };
+  const resetBoard = () => {
+    board = [];
+    for (let i = 0; i < grid; i++) {
+      board[i] = [];
+      for (let j = 0; j < grid; j++) {
+        board[i].push(Cell());
+      }
+    }
+  };
 
-  return { getBoard, placeMark, printBoard };
+  return { getBoard, placeMark, printBoard, resetBoard };
 }
 
 function Cell() {
@@ -58,17 +67,26 @@ function Player(name, value) {
   return { getName, getValue };
 }
 
-const GameController = (function () {
+const GameController = function () {
   const board = GameBoard();
 
-  const players = [Player("Player One", "X"), Player("Player Two", "O")];
+  let players = [Player("Player One", "X"), Player("Player Two", "O")];
 
   let activePlayer = players[0];
+
+  let winner = null;
 
   const switchTurns = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
-
+  const reset = () => {
+    activePlayer = players[0];
+    winner = null;
+    board.resetBoard();
+  };
+  const setPlayers = (newPlayers) => {
+    players = newPlayers;
+  };
   const getActivePlayer = () => activePlayer;
 
   const printRound = (winState) => {
@@ -82,8 +100,9 @@ const GameController = (function () {
     }
   };
   const playRound = (row, column) => {
+    let winCheck = null;
     if (board.placeMark(row, column, getActivePlayer().getValue())) {
-      const winCheck = checkWin(getActivePlayer());
+      winCheck = checkWin(getActivePlayer());
       if (winCheck === 1) {
         printRound("win");
       } else if (winCheck === 0) {
@@ -95,34 +114,56 @@ const GameController = (function () {
     } else {
       console.log("A mark is already there, try a different place!");
     }
+
+    return winCheck;
   };
 
-  const checkWin = (player) => {
-    const gameBoard = board.getBoard();
-    //Rows and Columns
+  const checkRows = (board, player) => {
     for (let i = 0; i < 3; i++) {
       if (
-        gameBoard[i].every((cell) => cell.getValue === player.getValue()) ||
-        [gameBoard[0][i], gameBoard[1][i], gameBoard[2][i]].every(
+        [board[i][0], board[i][1], board[i][2]].every(
           (cell) => cell.getValue() === player.getValue()
         )
       ) {
-        return 1;
+        return true;
       }
     }
-
-    //Diagonals
+    return false;
+  };
+  const checkCols = (board, player) => {
+    for (let i = 0; i < 3; i++) {
+      if (
+        [board[0][i], board[1][i], board[2][i]].every(
+          (cell) => cell.getValue() === player.getValue()
+        )
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+  const checkDiag = (board, player) => {
     if (
-      [gameBoard[0][0], gameBoard[1][1], gameBoard[2][2]].every(
+      [board[0][0], board[1][1], board[2][2]].every(
         (cell) => cell.getValue() === player.getValue()
       ) ||
-      [gameBoard[0][2], gameBoard[1][1], gameBoard[2][0]].every(
+      [board[0][2], board[1][1], board[2][0]].every(
         (cell) => cell.getValue() === player.getValue()
       )
     ) {
+      return true;
+    }
+    return false;
+  };
+  const checkWin = (player) => {
+    const gameBoard = board.getBoard();
+    if (
+      checkCols(gameBoard, player) ||
+      checkRows(gameBoard, player) ||
+      checkDiag(gameBoard, player)
+    ) {
       return 1;
     }
-
     //Tie Check
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
@@ -139,5 +180,87 @@ const GameController = (function () {
   return {
     playRound,
     getActivePlayer,
+    getBoard: board.getBoard,
+    reset,
+    setPlayers,
   };
-})();
+};
+
+const ScreenController = () => {
+  const gameController = GameController();
+  const infoDiv = document.querySelector(".info");
+  const boardDiv = document.querySelector(".board");
+  const startButton = document.querySelector(".start");
+  const playerOneInput = document.querySelector(".playerOne");
+  const playerTwoInput = document.querySelector(".playerTwo");
+
+  const updateScreen = (gameState) => {
+    boardDiv.textContent = "";
+
+    const board = gameController.getBoard();
+    const activePlayer = gameController.getActivePlayer();
+
+    if (gameState === "win") {
+      infoDiv.textContent = `${activePlayer.getName()} is the winner!`;
+      boardDiv.removeEventListener("click", boardClickHandler);
+    } else if (gameState === "tie") {
+      infoDiv.textContent = `It's a tie! No more moves available.`;
+      boardDiv.removeEventListener("click", boardClickHandler);
+    } else {
+      infoDiv.textContent = `${activePlayer.getName()}'s turn...`;
+    }
+
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        const cellButton = document.createElement("button");
+        cellButton.classList.add("cell");
+        cellButton.dataset.column = colIndex;
+        cellButton.dataset.row = rowIndex;
+        cellButton.textContent = cell.getValue() === 0 ? "" : cell.getValue();
+        boardDiv.appendChild(cellButton);
+      });
+    });
+  };
+
+  const boardClickHandler = (e) => {
+    const selectedColumn = e.target.dataset.column;
+    const selectedRow = e.target.dataset.row;
+    if (!selectedColumn && !selectedRow) return;
+
+    let result = gameController.playRound(selectedRow, selectedColumn);
+    if (result === 1) {
+      updateScreen("win");
+    } else if (result === 0) {
+      updateScreen("tie");
+    } else {
+      updateScreen("continue");
+    }
+  };
+  const startNewGame = () => {
+    gameController.setPlayers([
+      Player(
+        playerOneInput.value === "" ? "Player One" : playerOneInput.value,
+        "X"
+      ),
+      Player(
+        playerTwoInput.value === "" ? "Player Two" : playerTwoInput.value,
+        "O"
+      ),
+    ]);
+    gameController.reset();
+
+    playerOneInput.value = "";
+    playerTwoInput.value = "";
+    updateScreen();
+    if (startButton.textContent === "Start") {
+      startButton.textContent = "Reset";
+    }
+    boardDiv.removeEventListener("click", boardClickHandler);
+    boardDiv.addEventListener("click", boardClickHandler);
+  };
+
+  boardDiv.addEventListener("click", boardClickHandler);
+  startButton.addEventListener("click", startNewGame);
+};
+
+ScreenController();
